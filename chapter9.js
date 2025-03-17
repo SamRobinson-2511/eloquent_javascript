88d1b2589c058f4b7c3e6170a3d7365b36e4ef7f
Sam Robinson <robinson.will.sam@gmail.com>
2025-03-17 17:52:56 -0500

    chapter 9

--- chapter9.js
+++ chapter9.js
@@ -11,3 +11,14 @@ console.log(/abc/.test("abxde"));
 
 //Sets of Characters
 console.log(/[0123456789]/.test("in 1992")); 
+console.log(/[0-9]/.test("in 1992"))
+
+let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
+console.log(dateTime.test("01-30-2003 15:20"))
+console.log(dateTime.test("30-jan-2003 15:20"))
+
+let notBinary = /[^01]/; 
+console.log(notBinary.test("101001000100010"))
+console.log(notBinary.test("10002000101001"))
+
+//Repeating Parts of a Pattern
