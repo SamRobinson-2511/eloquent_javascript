c74c1eb7d279a1394d8781808954f491e0588ae4
Sam <robinson.will.sam@gmail.com>
2025-03-19 15:21:11 -0500

    more regex

--- chapter9.js
+++ chapter9.js
@@ -75,3 +75,31 @@ console.log(animalCount.test("15 pigs"));
 console.log(animalCount.test("15 pigchickens")); 
 
 //The Mechanics of Matching
+
+//Backtracking
+
+//The Replace Method
+console.log("papa".replace("p", "m")); 
+
+console.log("Borobudur".replace(/[ou]/, "a")); 
+console.log("Borobudur".replace(/[ou]/g, "a")); 
+console.log("Liskov", "Barbara\nMcCarthy", "John\nWadler", "Philip"
+  .replace(/(\w+), (\w+)/g, "$2", "$1"))
+
+
+  let s = "the cia and fbi"; 
+  console.log(s.replace(/\b(fbi|cia)\b/g, str=> str.toUpperCase()))
+
+  let stock = "1 lemon, 2 cabbages, and 101 eggs"; 
+  function minusOne(match, amount, unit){
+    amount = Number(amount)-1;
+    if (amount ==1){
+      unit = unit.slice(0, unit.length - 1); 
+    } else if (amount == 0){
+      amount = "no"; 
+    }
+    return amount + " " + unit;
+  }
+  console.log(stock.replace(/(\d+)(\w+)/g, minusOne)); 
+  
+  
